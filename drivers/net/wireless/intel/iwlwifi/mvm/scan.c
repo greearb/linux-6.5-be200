greearb/linux-ct-5.17

Revert "iwlwifi: mvm: Passively scan non PSC channels only when requested so"
This reverts commit c9e61b28c4c460f1e08578f40043ba96f643579e.

This commit breaks association to wifi-6E Cisco AP on frequencey 5995

drivers/net/wireless/intel/iwlwifi/mvm/scan.c
@@ -1718,36 +1718,26 @@ iwl_mvm_umac_scan_fill_6g_chan_list(struct iwl_mvm *mvm,
 }
 
 /* TODO: this function can be merged with iwl_mvm_scan_umac_fill_ch_p_v6 */
-static u32
-iwl_mvm_umac_scan_cfg_channels_v6_6g(struct iwl_mvm *mvm,
-				     struct iwl_mvm_scan_params *params,
+static void
+iwl_mvm_umac_scan_cfg_channels_v6_6g(struct iwl_mvm_scan_params *params,
 				     u32 n_channels,
 				     struct iwl_scan_probe_params_v4 *pp,
 				     struct iwl_scan_channel_params_v6 *cp,
 				     enum nl80211_iftype vif_type)
 {
+	struct iwl_scan_channel_cfg_umac *channel_cfg = cp->channel_config;
 	int i;
 	struct cfg80211_scan_6ghz_params *scan_6ghz_params =
 		params->scan_6ghz_params;
-	u32 ch_cnt;
 
-	for (i = 0, ch_cnt = 0; i < params->n_channels; i++) {
+	for (i = 0; i < params->n_channels; i++) {
 		struct iwl_scan_channel_cfg_umac *cfg =
-			&cp->channel_config[ch_cnt];
+			&cp->channel_config[i];
 
 		u32 s_ssid_bitmap = 0, bssid_bitmap = 0, flags = 0;
 		u8 j, k, s_max = 0, b_max = 0, n_used_bssid_entries;
 		bool force_passive, found = false, allow_passive = true,
 		     unsolicited_probe_on_chan = false, psc_no_listen = false;
-
-		/*
-		 * Avoid performing passive scan on non PSC channels unless the
-		 * scan is specifically a passive scan, i.e., no SSIDs
-		 * configured in the scan command.
-		 */
-		if (!cfg80211_channel_is_psc(params->channels[i]) &&
-		    !params->n_6ghz_params && params->n_ssids)
-			continue;
 
 		cfg->v1.channel_num = params->channels[i]->hw_value;
 		cfg->v2.band = 2;
@@ -1868,16 +1858,8 @@ iwl_mvm_umac_scan_cfg_channels_v6_6g(struct iwl_mvm *mvm,
 		else
 			flags |= bssid_bitmap | (s_ssid_bitmap << 16);
 
-		cfg->flags |= cpu_to_le32(flags);
-		ch_cnt++;
+		channel_cfg[i].flags |= cpu_to_le32(flags);
 	}
-
-	if (params->n_channels > ch_cnt)
-		IWL_DEBUG_SCAN(mvm,
-			       "6GHz: reducing number channels: (%u->%u)\n",
-			       params->n_channels, ch_cnt);
-
-	return ch_cnt;
 }
 
 static u8 iwl_mvm_scan_umac_chan_flags_v2(struct iwl_mvm *mvm,
@@ -2467,14 +2449,10 @@ static int iwl_mvm_scan_umac_v14_and_above(struct iwl_mvm *mvm,
 
 	iwl_mvm_umac_scan_fill_6g_chan_list(mvm, params, pb);
 
-	cp->count = iwl_mvm_umac_scan_cfg_channels_v6_6g(mvm, params,
-							 params->n_channels,
-							 pb, cp, vif->type);
-	if (!cp->count) {
-		mvm->scan_uid_status[uid] = 0;
-		return -EINVAL;
-	}
-
+	iwl_mvm_umac_scan_cfg_channels_v6_6g(params,
+					     params->n_channels,
+					     pb, cp, vif->type);
+	cp->count = params->n_channels;
 	if (!params->n_ssids ||
 	    (params->n_ssids == 1 && !params->ssids[0].ssid_len))
 		cp->flags |= IWL_SCAN_CHANNEL_FLAG_6G_PSC_NO_FILTER;

greearb/linux-6.5-be200

drivers/net/wireless/intel/iwlwifi/mvm/scan.c
@@ -1731,39 +1731,29 @@ iwl_mvm_umac_scan_fill_6g_chan_list(struct iwl_mvm *mvm,
 	pp->bssid_num = idex_b;
 }
 
-/* TODO: this function can be merged with iwl_mvm_scan_umac_fill_ch_p_v7 */
-static u32
-iwl_mvm_umac_scan_cfg_channels_v7_6g(struct iwl_mvm *mvm,
-				     struct iwl_mvm_scan_params *params,
+/* TODO: this function can be merged with iwl_mvm_scan_umac_fill_ch_p_v6 */
+static void
+iwl_mvm_umac_scan_cfg_channels_v7_6g(struct iwl_mvm_scan_params *params,
 				     u32 n_channels,
 				     struct iwl_scan_probe_params_v4 *pp,
 				     struct iwl_scan_channel_params_v7 *cp,
 				     enum nl80211_iftype vif_type,
 				     u32 version)
 {
+	struct iwl_scan_channel_cfg_umac *channel_cfg = cp->channel_config;
 	int i;
 	struct cfg80211_scan_6ghz_params *scan_6ghz_params =
 		params->scan_6ghz_params;
-	u32 ch_cnt;
 
-	for (i = 0, ch_cnt = 0; i < params->n_channels; i++) {
+	for (i = 0; i < params->n_channels; i++) {
 		struct iwl_scan_channel_cfg_umac *cfg =
-			&cp->channel_config[ch_cnt];
+			&cp->channel_config[i];
 
 		u32 s_ssid_bitmap = 0, bssid_bitmap = 0, flags = 0;
 		u8 j, k, s_max = 0, b_max = 0, n_used_bssid_entries;
 		bool force_passive, found = false, allow_passive = true,
 		     unsolicited_probe_on_chan = false, psc_no_listen = false;
 		s8 psd_20 = IEEE80211_RNR_TBTT_PARAMS_PSD_RESERVED;
-
-		/*
-		 * Avoid performing passive scan on non PSC channels unless the
-		 * scan is specifically a passive scan, i.e., no SSIDs
-		 * configured in the scan command.
-		 */
-		if (!cfg80211_channel_is_psc(params->channels[i]) &&
-		    !params->n_6ghz_params && params->n_ssids)
-			continue;
 
 		cfg->v1.channel_num = params->channels[i]->hw_value;
 		if (version < 17)
@@ -1902,19 +1892,11 @@ iwl_mvm_umac_scan_cfg_channels_v7_6g(struct iwl_mvm *mvm,
 		else
 			flags |= bssid_bitmap | (s_ssid_bitmap << 16);
 
-		cfg->flags |= cpu_to_le32(flags);
 		if (version >= 17)
 			cfg->v5.psd_20 = psd_20;
 
-		ch_cnt++;
+		channel_cfg[i].flags |= cpu_to_le32(flags);
 	}
-
-	if (params->n_channels > ch_cnt)
-		IWL_DEBUG_SCAN(mvm,
-			       "6GHz: reducing number channels: (%u->%u)\n",
-			       params->n_channels, ch_cnt);
-
-	return ch_cnt;
 }
 
 
@@ -2549,15 +2531,10 @@ static int iwl_mvm_scan_umac_v14_and_above(struct iwl_mvm *mvm,
 
 	iwl_mvm_umac_scan_fill_6g_chan_list(mvm, params, pb);
 
-	cp->count = iwl_mvm_umac_scan_cfg_channels_v7_6g(mvm, params,
-							 params->n_channels,
-							 pb, cp, vif->type,
-							 version);
-	if (!cp->count) {
-		mvm->scan_uid_status[uid] = 0;
-		return -EINVAL;
-	}
-
+	iwl_mvm_umac_scan_cfg_channels_v7_6g(params,
+					     params->n_channels,
+					     pb, cp, vif->type, version);
+	cp->count = params->n_channels;
 	if (!params->n_ssids ||
 	    (params->n_ssids == 1 && !params->ssids[0].ssid_len))
 		cp->flags |= IWL_SCAN_CHANNEL_FLAG_6G_PSC_NO_FILTER;