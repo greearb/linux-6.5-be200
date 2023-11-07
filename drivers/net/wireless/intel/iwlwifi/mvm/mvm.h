c8f99acb915aa2fe139f5d9e288e8071871f3422
Rory Little <rory.little@candelatech.com>
2023-11-07 13:37:43 -0800

    iwlwifi: Added block_traffic debugfs file to optionally block all incoming tx/rx packets from entering/leaving the mac80211 stack for mvm drivers.
    This follows a similar implementation for mt76 drivers. Rx blocking for
    non-multiqueue drivers is untested.
    
    Signed-off-by: Rory Little <rory.little@candelatech.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -1357,6 +1357,10 @@ struct iwl_mvm {
 	bool rfi_wlan_master;
 	bool force_enable_rfi;
 	bool statistics_clear;
+
+#define IWL_MVM_BLOCK_TX 0x1
+#define IWL_MVM_BLOCK_RX 0x2
+	u8 block_traffic;
 };
 
 /* Extract MVM priv from op_mode and _hw */
